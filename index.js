b94260909fd3e93c1a6d3ec18c01fd42ea8ef448
Thạch Nguyễn <thachnh.tesse@gmail.com>
2021-10-21 14:50:47 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const session = require('express-session')
 const methodOverride = require('method-override')
 
